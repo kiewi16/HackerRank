593bcaeef8660a316953c592a4e3e3ac0903266b
Kim Ewing <pekar.kimberly@gmail.com>
2024-12-02 15:07:55 -0700

    revises pseudocode

--- completed/lonely-integer.js
+++ completed/lonely-integer.js
@@ -9,14 +9,13 @@ let numbers = [1,2,3,4,3,2,1]
         // iterate through the array of integers and find integer that occurs ONCE
             // use reduce to create a new object where the key is the integer and the value is the number of times the integer appears
         // the key that has a value of one is the unique integer 
-        // declare a variable called integerCounts
+        // declare a variable called integersAndCounts
             // access the values using Object.Entries
-        // flatten the Object.Entries because it will return an array of arrays
-            // declare a variable integersAndCounts
-                // use forEach to iterate through integersAndCounts, include index as an argument 
-                // if number is equal to 1, return the index position of that number
-        // return integersAndCounts[indexPosition - 1]
-
+            // flatten the Object.Entries because it will return an array of arrays
+            // declare a variable loneInteger
+            // use forEach to iterate through integersAndCounts, include index as an argument 
+                // if number is equal to 1, push integersAndCounts at index position minus 1 to loneInteger
+        // return loneInteger make sure to parseInt because value pushed to loneInteger will be a string
 // output: one integer
 
 function returnLoneInteger(numbers) {
